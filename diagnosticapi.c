8b969386be314f569ef55eb77632a84d87654a42
dhruva15026 <dhruva15026@iiitd.ac.in>
2017-04-22 22:32:56 +0530

    Update diagnosticapi.c

--- diagnosticapi.c
+++ diagnosticapi.c
@@ -1,11 +1,15 @@
-#include "diagnosticapi.h"
-#include ""
+#include <diagnosticapi.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 
 // SFS FILE STRUCTURE DEFINITION IN Bytes 
 #define  start_super_block 0 // super block size is 4KB
 #define  start_i_bitmap 4*1024 // Inode Bit Map - 4KB Size
 #define  start_d_bitmap 4*1024 + 4*1024 // Data Bit Map - 4KB Size
-#define  start_i_data 4*1024 + 4*1024 + 4*1024	// Inode Data - No of Inode* Size of Inode 
+#define  start_i_data 4*1024 + 4*1024 + 4*1024	// Inode Data - No of Inode* Size of Inode =80  
 #define  start_data 4*1024 + 4*1024 + 4*1024 +				// Rest of the defined filesystem
 
 #define  block_size 4*1024 
@@ -13,7 +17,27 @@
 
 int createSFS( char* filename, int nbytes) // returns a fileSystemId 
 {
+	int i;
 
+
+	int sfs_file_descriptor=	open( filename, O_WRONLY | O_CREAT | O_TRUNC,S_IRUSR | S_IWUSR | S_IRGRP );	//Creating SFS in the specified file
+	if (sfs_file_descriptor==-1)
+	{
+		perror("Cannot create SFS \nExiting....\n"); 
+		exit(1);
+	}
+	i=nbytes;
+
+	while(i>0)
+	{
+		int val = write(sfs_file_descriptor,'\0', 1);				// Initialize the file with null;
+		if (val<=0)
+		{
+			printf("Cant Initialize the file\n");
+			exit(1);
+		}
+	}
+	return sfs_file_descriptor;
 }
 int readData( int disk, int blockNum, void* block) // returns the num. of bytes read and read them into a buffer pointed by block. 
 {
