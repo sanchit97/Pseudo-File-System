2b5b5a93160863a1a07320e621c69d268829aacf
dhruva15026 <dhruva15026@iiitd.ac.in>
2017-04-25 11:45:19 +0530

    Update fileapi.c

--- fileapi.c
+++ fileapi.c
@@ -1,36 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-//file api functions here
-#include "fileapi.h"
+#include <diagnosticapi.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 
-// SFS FILE STRUCTURE DEFINITION IN Bytes
+// SFS FILE STRUCTURE DEFINITION IN Bytes 
 #define  start_super_block 0 // super block size is 4KB
 #define  start_i_bitmap 4*1024 // Inode Bit Map - 4KB Size
 #define  start_d_bitmap 4*1024 + 4*1024 // Data Bit Map - 4KB Size
-#define  start_i_data 4*1024 + 4*1024 + 4*1024	// Inode Data - No of Inode* Size of Inode =80
-#define  start_data 4*1024 + 4*1024 + 4*1024 +				// Rest of the defined filesystem
+#define  start_i_data 4*1024 + 4*1024 + 4*1024	// Inode Data - No of Inode 512 * Size of Inode 24 = 3*4*1024= 12KB
+#define  start_data 4*1024 + 4*1024 + 4*1024 +	12*1024			// Rest of the defined filesystem
+#define  block_size 4*1024 
 
-#define  block_size 4*1024
-
-
-int createSFS( char* filename, int nbytes) // returns a fileSystemId
+struct inode     //size is 24 bytes
+{
+	char name[12];
+	int  start_block;  // data block starting from address start_data 0-...
+	int no_of_blocks;
+	int size_of_file; //in bytes
+};
+int createSFS( char* filename, int nbytes) // returns a fileSystemId 
 {
 	int i;
 
-	int sfs_file_descriptor=open(filename, O_WRONLY | O_CREAT | O_TRUNC,S_IRUSR | S_IWUSR | S_IRGRP );//Creating SFS in the specified file
+
+	int sfs_file_descriptor=	open( filename, O_WRONLY | O_CREAT | O_TRUNC,S_IRUSR | S_IWUSR | S_IRGRP );	//Creating SFS in the specified file
 	if (sfs_file_descriptor==-1)
 	{
-		perror("Cannot create SFS \nExiting....\n");
+		perror("Cannot create SFS \nExiting....\n"); 
 		exit(1);
 	}
-
 	i=nbytes;
 
 	while(i>0)
@@ -44,22 +44,22 @@ int createSFS( char* filename, int nbytes) // returns a fileSystemId
 	}
 	return sfs_file_descriptor;
 }
-int readData( int disk, int blockNum, void* block) // returns the num. of bytes read and read them into a buffer pointed by block.
+int readData( int disk, int blockNum, void* block) // returns the num. of bytes read and read them into a buffer pointed by block. 
 {
 
 }
 
-int writeData(int disk, int blockNum, void* block) //writes that data in the block to the disk block blockNum.
+int writeData(int disk, int blockNum, void* block) //writes that data in the block to the disk block blockNum. 
 {
 
 }
 
-int writeFile(int disk, char* filename, void* block) //write the buffer pointed by block. The same data should be recoverable by the file name.
+int writeFile(int disk, char* filename, void* block) //write the buffer pointed by block. The same data should be recoverable by the file name. 
 {
 
 }
 
 int readFile(int disk, char* filename, void* block) //returns the data stored by the given file name.
 {
-
+	
 }
